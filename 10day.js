44dd8542365109d75776d34fb47416a48549063f
Anurag <as6400263@gmail.com>
2025-07-21 20:09:12 +0530

    Completed Day 10

--- 10day.js
+++ 10day.js
@@ -1,3 +1,12 @@
+/* 🟠  2666. Allow One Function Call
+
+Given a function fn, return a new function that is identical to the original function except that it ensures fn is called at most once.
+
+The first time the returned function is called, it should return the same result as fn.
+Every subsequent time it is called, it should return undefined.🟠 */
+
+/* Solution 👇 */
+
 let fn = (a, b, c) => a + b + c;
 
 var once = function (fn) {
@@ -18,3 +27,6 @@ var once = function (fn) {
 const onceFn = once(fn);
 console.log(onceFn(1, 2, 3));
 console.log(onceFn(2, 3, 6));
+
+/* ✅ Real-World Example:
+Imagine you have a "submit" button that should only work once to avoid double submissions. You’d want the submit function to run only once, even if the user clicks many times */
